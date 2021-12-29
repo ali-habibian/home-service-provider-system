0abf8b29b69f3ff407a0cbfcdbaf356b11a2befc
Ali Habibian <ali.habibian04@gmail.com>
2021-12-29 15:56:43 +0330

    Implemented MainCategory service

---
+++ src/test/java/ir/maktab/hsps/service/MainCategoryServiceTest.java
@@ -0,0 +1,24 @@
+package ir.maktab.hsps.service;
+
+import ir.maktab.hsps.entity.category.MainCategory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MainCategoryServiceTest {
+
+    @Autowired
+    private MainCategoryService mainCategoryService;
+
+    @Test
+    void test_save(){
+        MainCategory mainCategory = new MainCategory();
+        mainCategory.setName("MainCategory-2");
+
+        MainCategory result = mainCategoryService.save(mainCategory);
+        assertNotNull(result);
+    }
+}
