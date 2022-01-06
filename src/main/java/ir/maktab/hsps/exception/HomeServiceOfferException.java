5ef0a94f2489c6c5e272aaa7ec6716be3a2bb8db
Ali Habibian <ali.habibian04@gmail.com>
2022-01-07 01:59:15 +0330

    Updated HomeServiceOfferService sendOffer function

---
+++ src/main/java/ir/maktab/hsps/exception/HomeServiceOfferException.java
@@ -0,0 +1,10 @@
+package ir.maktab.hsps.exception;
+
+public class HomeServiceOfferException extends RuntimeException{
+    public HomeServiceOfferException() {
+    }
+
+    public HomeServiceOfferException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ir/maktab/hsps/service/HomeServiceOfferService.java
+++ src/main/java/ir/maktab/hsps/service/HomeServiceOfferService.java
@@ -1,11 +1,14 @@
 package ir.maktab.hsps.service;
 
 import ir.maktab.hsps.entity.HomeServiceOffer;
+import ir.maktab.hsps.entity.category.SubCategory;
+import ir.maktab.hsps.exception.HomeServiceOfferException;
 import ir.maktab.hsps.repository.HomeServiceOfferRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,21 @@ public class HomeServiceOfferService extends BaseService<HomeServiceOffer, Long>
     @PostConstruct
     public void init() {
         setJpaRepository(homeServiceOfferRepository);
+    }
+
+    public HomeServiceOffer sendOffer(HomeServiceOffer homeServiceOffer) {
+        Set<SubCategory> proficientSubCategories = homeServiceOffer.getProficient().getSubCategories();
+        SubCategory orderSubCategory = homeServiceOffer.getHomeServiceOrder().getSubCategory();
+        if (!proficientSubCategories.contains(orderSubCategory)) {
+            throw new HomeServiceOfferException("The proficient does not have the necessary expertise");
+        }
+
+        Double proficientSuggestedPrice = homeServiceOffer.getSuggestedPrice();
+        Double orderSuggestedPrice = homeServiceOffer.getHomeServiceOrder().getSuggestedPrice();
+        if (proficientSuggestedPrice < orderSuggestedPrice) {
+            throw new HomeServiceOfferException("Proficient suggested price most not be less than order suggested price");
+        }
+
+        return super.save(homeServiceOffer);
     }
 }
